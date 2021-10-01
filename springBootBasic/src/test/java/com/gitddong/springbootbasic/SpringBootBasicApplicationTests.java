6645cc62e62f85a8d454f7be7ffe7448b9aa06a2
hhaggang22 <20181018@sungshin.ac.kr>
2021-10-01 18:09:01 +0900

    settings

---
+++ springBootBasic/src/test/java/com/gitddong/springbootbasic/SpringBootBasicApplicationTests.java
@@ -0,0 +1,11 @@
+package com.gitddong.springbootbasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest class SpringBootBasicApplicationTests {
+
+    @Test void contextLoads() {
+    }
+
+}
